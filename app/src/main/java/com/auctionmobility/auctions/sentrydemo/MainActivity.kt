0e8dae6d1d8915038de6a555a2246a0398ef280f
Ashvin <ashvin.v@simformsolutions.com>
2025-07-01 12:13:46 +0530

    Added null point on click of button

--- app/src/main/java/com/auctionmobility/auctions/sentrydemo/MainActivity.kt
+++ app/src/main/java/com/auctionmobility/auctions/sentrydemo/MainActivity.kt
@@ -22,14 +22,14 @@ import io.sentry.Sentry
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//    // waiting for view to draw to better represent a captured error with a screenshot
-//    findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
-//      try {
-//        throw Exception("This app uses Sentry! :)")
-//      } catch (e: Exception) {
-//        Sentry.captureException(e)
-//      }
-//    }
+    // waiting for view to draw to better represent a captured error with a screenshot
+    findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
+      try {
+        throw Exception("This app uses Sentry! :)")
+      } catch (e: Exception) {
+        Sentry.captureException(e)
+      }
+    }
 
         enableEdgeToEdge()
         setContent {
