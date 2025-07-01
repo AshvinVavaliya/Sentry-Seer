57b5856803005a3f74f4c2e6741394530ec3912a
Ashvin <ashvin.v@simformsolutions.com>
2025-07-01 12:03:38 +0530

    Added null point on click of button

--- app/src/main/java/com/auctionmobility/auctions/sentrydemo/MainActivity.kt
+++ app/src/main/java/com/auctionmobility/auctions/sentrydemo/MainActivity.kt
@@ -4,27 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.auctionmobility.auctions.sentrydemo.ui.theme.SentryDemoTheme
 import io.sentry.Sentry
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    // waiting for view to draw to better represent a captured error with a screenshot
-    findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
-      try {
-        throw Exception("This app uses Sentry! :)")
-      } catch (e: Exception) {
-        Sentry.captureException(e)
-      }
-    }
+//    // waiting for view to draw to better represent a captured error with a screenshot
+//    findViewById<android.view.View>(android.R.id.content).viewTreeObserver.addOnGlobalLayoutListener {
+//      try {
+//        throw Exception("This app uses Sentry! :)")
+//      } catch (e: Exception) {
+//        Sentry.captureException(e)
+//      }
+//    }
 
         enableEdgeToEdge()
         setContent {
@@ -42,10 +47,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "Hello $name!")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            val willCrash: String? = null
+            val result = willCrash!!.length // Null crash on button click
+        }) {
+            Text("Trigger Null Crash")
+        }
+    }
 }
 
 @Preview(showBackground = true)
